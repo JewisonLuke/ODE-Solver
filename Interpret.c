6da4352e6e0bb1244b35cb1829ed35132ae92684
JewisonLuke <lukewj2@illinois.edu>
2026-09-05 16:39:48 -0500

    new file

---
+++ Interpret.c
@@ -0,0 +1,4 @@
+void interpret(char *input) {
+    input[0] = '0';
+    return;
+}
--- Main.c
+++ Main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include "Interpret.c"
 
 #define INPUT_SIZE 100
 
 int main() {
     char input[INPUT_SIZE];
+
+    int inputIdxNum = INPUT_SIZE;
     
     printf("Enter ODE: ");
     scanf("%s", input);
+
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        if (input[i] == '\0') {
+            inputIdxNum = i;
+            break;
+        }
+    } //finds which value is the last in the array, then I don't have to search through all indexes that aren't filled
+
+    if (inputIdxNum == INPUT_SIZE) {
+        printf("Input too large\n");
+        return 1;
+    }
+
+    interpret(input);
+
     printf("%s\n", input);
 
     return 0;
